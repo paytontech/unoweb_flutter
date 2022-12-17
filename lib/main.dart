8a67f2d804dbb636684e46a5b4015682ca2cfc7c
paytondev <81662879+paytontech@users.noreply.github.com>
2022-12-17 12:15:10 -0500

    woohoo! special cards work now!! woohoo!

--- lib/main.dart
+++ lib/main.dart
@@ -55,15 +55,33 @@ class _MyHomePageState extends State<MyHomePage> {
     for (var i = 0; i < 10; i++) {
       cards.add({'color': 'red', 'number': i, 'special': false});
     }
+    //colored special (+4, +2, reverse)
+    cards.add({'color': 'red', 'type': '+2', 'special': true});
+    cards.add({'color': 'red', 'type': '+4', 'special': true});
     for (var i = 0; i < 10; i++) {
       cards.add({'color': 'blue', 'number': i, 'special': false});
     }
+    cards.add({'color': 'blue', 'type': '+2', 'special': true});
+    cards.add({'color': 'blue', 'type': '+4', 'special': true});
     for (var i = 0; i < 10; i++) {
       cards.add({'color': 'green', 'number': i, 'special': false});
     }
+    cards.add({'color': 'green', 'type': '+2', 'special': true});
+    cards.add({'color': 'green', 'type': '+4', 'special': true});
     for (var i = 0; i < 10; i++) {
       cards.add({'color': 'yellow', 'number': i, 'special': false});
     }
+    cards.add({'color': 'yellow', 'type': '+2', 'special': true});
+    cards.add({'color': 'yellow', 'type': '+4', 'special': true});
+    //wild cards
+    cards.add({
+      'color': 'wild',
+      'type': 'normal',
+      'special': true,
+      'chosenColor': ''
+    });
+    cards.add(
+        {'color': 'wild', 'type': '+4', 'special': true, 'chosenColor': ''});
   }
 
   @override
@@ -74,17 +92,34 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Color getCardColor(card) {
-    switch (card['color']) {
-      case 'red':
-        return (Colors.red);
-      case 'blue':
-        return (Colors.blue);
-      case 'green':
-        return (Colors.green);
-      case 'yellow':
-        return (Color.fromARGB(255, 153, 138, 0));
-      default:
-        return (Colors.black);
+    if (!(card['color'] == 'wild')) {
+      switch (card['color']) {
+        case 'red':
+          return (Colors.red);
+        case 'blue':
+          return (Colors.blue);
+        case 'green':
+          return (Colors.green);
+        case 'yellow':
+          return (Color.fromARGB(255, 153, 138, 0));
+        default:
+          return (Colors.black);
+      }
+    } else if (card['color'] == 'wild') {
+      switch (card['chosenColor']) {
+        case 'red':
+          return (Colors.red);
+        case 'blue':
+          return (Colors.blue);
+        case 'green':
+          return (Colors.green);
+        case 'yellow':
+          return (Color.fromARGB(255, 153, 138, 0));
+        default:
+          return (Colors.black);
+      }
+    } else {
+      return Colors.black;
     }
   }
 
@@ -95,7 +130,9 @@ class _MyHomePageState extends State<MyHomePage> {
       List<Map> possibleCards = [];
       for (var card in bot['cards']) {
         if (card['color'] == gameData['stack']['current']['color'] ||
-            card['number'] == gameData['stack']['current']['number']) {
+            card['number'] == gameData['stack']['current']['number'] ||
+            (card['special'] && card['color'] == 'wild') ||
+            (card['color'] == gameData['stack']['current']['chosenColor'])) {
           possibleCards.add(card);
         }
       }
@@ -107,8 +144,36 @@ class _MyHomePageState extends State<MyHomePage> {
       } else {
         //cards available
         await Future.delayed(const Duration(seconds: 3));
-        playCard(
-            possibleCards[Random().nextInt(possibleCards.length)], bot['id']);
+        Map chosenCard = possibleCards[Random().nextInt(possibleCards.length)];
+        Map checkedCard = chosenCard;
+        if (chosenCard['color'] == 'wild') {
+          int red = 0;
+          int blue = 0;
+          int green = 0;
+          int yellow = 0;
+          for (var card in bot['cards']) {
+            switch (card['color']) {
+              case 'red':
+                red += 1;
+                break;
+              case 'blue':
+                blue += 1;
+                break;
+              case 'green':
+                green += 1;
+                break;
+              case 'yellow':
+                yellow += 1;
+                break;
+            }
+            int highest = [red, blue, green, yellow].reduce(max);
+            if (red == highest) checkedCard['chosenColor'] = 'red';
+            if (blue == highest) checkedCard['chosenColor'] = 'blue';
+            if (yellow == highest) checkedCard['chosenColor'] = 'yellow';
+            if (green == highest) checkedCard['chosenColor'] = 'green';
+          }
+        }
+        playCard(checkedCard, bot['id']);
       }
     } else {
       print("bot cannot play!");
@@ -120,8 +185,28 @@ class _MyHomePageState extends State<MyHomePage> {
     if (playerID == gameData['currentPlayer']) {
       //valid
       if (card['color'] == gameData['stack']['current']['color'] ||
-          card['number'] == gameData['stack']['current']['number']) {
+          card['number'] == gameData['stack']['current']['number'] ||
+          (card['special'] && card['color'] == 'wild') ||
+          (card['color'] == gameData['stack']['current']['chosenColor'])) {
         //valid
+        if (card['special'] && card['type'] != "normal") {
+          if (card['type'] == '+2') {
+            print("+2 card, giving 2 cards to ${getNextPlayer()}");
+            //draw 2
+            for (var i = 0; i < 2; i++) {
+              setState(() {
+                gameData['players'][getNextPlayer()]['cards']
+                    .add(cards[Random().nextInt(cards.length)]);
+              });
+            }
+          } else if (card['type'] == "+4") {
+            print("+4 card, giving 2 cards to ${getNextPlayer()}");
+            for (var i = 0; i < 4; i++) {
+              gameData['players'][getNextPlayer()]['cards']
+                  .add(cards[Random().nextInt(cards.length)]);
+            }
+          }
+        }
         setState(() {
           gameData['stack']['prev'].add(gameData['stack']['current']);
           gameData['stack']['current'] = card;
@@ -151,6 +236,14 @@ class _MyHomePageState extends State<MyHomePage> {
           invalidAttemptError = false;
         });
       }
+    }
+  }
+
+  int getNextPlayer() {
+    if (gameData['currentPlayer'] >= (gameData['players'].length - 1)) {
+      return 0;
+    } else {
+      return (gameData['currentPlayer']) + 1;
     }
   }
 
@@ -250,6 +343,7 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: Center(
+            child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
@@ -281,17 +375,71 @@ class _MyHomePageState extends State<MyHomePage> {
                     .map<Widget>((card) => ElevatedButton(
                           onPressed: gameData['currentPlayer'] == 0
                               ? () {
-                                  playCard(card, 0);
                                   print(gameData['players'][0]['cards']);
+                                  playCard(card, 0);
                                 }
                               : null,
                           style: ElevatedButton.styleFrom(
                               backgroundColor: getCardColor(card),
-                              minimumSize: Size(40, 100)),
-                          child: Text(
-                            card['color'] + "\n" + card['number'].toString(),
-                            textAlign: TextAlign.center,
-                          ),
+                              minimumSize: Size(50, 120)),
+                          child: !card['special']
+                              ? Text(
+                                  "${card['color']}\n${card['number'].toString()}",
+                                  textAlign: TextAlign.center,
+                                )
+                              : !(card['chosenColor'] == '')
+                                  ? Text(
+                                      "${card['color']}\n${card['type']}",
+                                      textAlign: TextAlign.center,
+                                    )
+                                  : Column(children: [
+                                      Text("${card['color']} ${card['type']}"),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            card['chosenColor'] = 'red';
+                                          });
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.red,
+                                            minimumSize: Size(80, 20)),
+                                        child: const Text("Red"),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            card['chosenColor'] = 'blue';
+                                          });
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.blue,
+                                            minimumSize: Size(80, 20)),
+                                        child: const Text("Blue"),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            card['chosenColor'] = 'green';
+                                          });
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Colors.green,
+                                            minimumSize: Size(80, 20)),
+                                        child: const Text("Green"),
+                                      ),
+                                      ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            card['chosenColor'] = 'yellow';
+                                          });
+                                        },
+                                        style: ElevatedButton.styleFrom(
+                                            backgroundColor: Color.fromARGB(
+                                                255, 153, 138, 0),
+                                            minimumSize: Size(80, 20)),
+                                        child: const Text("Yellow"),
+                                      ),
+                                    ]),
                         ))
                     .toList(),
               ),
@@ -319,12 +467,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     backgroundColor: getCardColor(gameData['stack']['current']),
                     minimumSize: Size(40, 100),
                     alignment: Alignment.center),
-                child: Text(
-                  gameData['stack']['current']['color'] +
-                      "\n" +
-                      gameData['stack']['current']['number'].toString(),
-                  textAlign: TextAlign.center,
-                ),
+                child: !gameData['stack']['current']['special']
+                    ? Text(
+                        "${gameData['stack']['current']['color']}\n${gameData['stack']['current']['number'].toString()}",
+                        textAlign: TextAlign.center,
+                      )
+                    : Text(
+                        "${gameData['stack']['current']['color']}\n${gameData['stack']['current']['type']}",
+                        textAlign: TextAlign.center,
+                      ),
               ),
               Wrap(
                 children: gameData['players']
@@ -343,7 +494,7 @@ class _MyHomePageState extends State<MyHomePage> {
               )
             ],
           ),
-        ), // This trailing comma makes auto-formatting nicer for build methods.
+        )), // This trailing comma makes auto-formatting nicer for build methods.
       );
     }
     return Container(child: game);
