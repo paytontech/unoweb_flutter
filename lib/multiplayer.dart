2d62fec6b91074764e7bb38c9e3e13b88f785dc8
paytondev <81662879+paytontech@users.noreply.github.com>
2022-12-17 14:55:31 -0500

    kinda sorta maybe working on MP

--- lib/multiplayer.dart
+++ lib/multiplayer.dart
@@ -3,49 +3,90 @@ import 'dart:async';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:web_socket_channel/web_socket_channel.dart';
 
 import 'package:flutter/material.dart';
-import 'multiplayer.dart';
 
-void main() {
-  runApp(const MyApp());
+class MultiplayerPageOne extends StatefulWidget {
+  const MultiplayerPageOne({super.key});
+
+  @override
+  State<MultiplayerPageOne> createState() => _MultiplayerPageOneState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class _MultiplayerPageOneState extends State<MultiplayerPageOne> {
+  String textCode = "";
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'unoweb-flutter',
-      theme: ThemeData(
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'unoweb-flutter'),
+    return Scaffold(
+      appBar: AppBar(title: const Text("Choose an option")),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const Text(
+              "Would you like to create a lobby, or join an already existing lobby?"),
+          const SizedBox(
+            height: 50,
+            width: 0,
+          ),
+          ElevatedButton(
+              onPressed: () {
+                //create a lobby
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => MultiplayerGame()));
+              },
+              child: Text("Create")),
+          const SizedBox(
+            height: 50,
+            width: 0,
+          ),
+          const Text("Join"),
+          Padding(
+            padding: EdgeInsets.all(100),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Enter a room code',
+              ),
+              onChanged: ((value) {
+                setState(() {
+                  setState(() {
+                    textCode = value;
+                  });
+                });
+              }),
+            ),
+          ),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => MultiplayerGame(
+                              gameCode: textCode,
+                            )));
+              },
+              child: const Text("Join"))
+        ],
+      )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class MultiplayerGame extends StatefulWidget {
+  const MultiplayerGame({super.key, this.gameCode});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final String? gameCode;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MultiplayerGame> createState() => _MultiplayerGameState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MultiplayerGameState extends State<MultiplayerGame> {
+  final db = FirebaseFirestore.instance;
+
+  Map clientState = {'host': false, 'username': ''};
   Map gameData = {
     'players': [],
     'currentPlayer': 0,
@@ -89,10 +130,19 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  void initState() {
+  void initState() async {
     super.initState();
-    genCards();
-    dealCards(4);
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    if (widget.gameCode == null) {
+      print("this client is host");
+      setState(() {
+        clientState['host'] = true;
+      });
+    } else {
+      print("This client is not a host!! woohoo!");
+    }
   }
 
   Color getCardColor(card) {
@@ -344,21 +394,13 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       game = Scaffold(
         appBar: AppBar(
-          title: Text(widget.title),
+          title: Text("unoweb"),
         ),
         body: Center(
             child: SingleChildScrollView(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              TextButton(
-                  onPressed: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const MultiplayerPageOne()));
-                  },
-                  child: const Text("New! Try Multiplayer [Beta]")),
               if (gameData['currentPlayer'] > 0)
                 Text("Current Player: ${gameData['currentPlayer']}"),
               if (gameData['currentPlayer'] == 0)
@@ -512,15 +554,3 @@ class _MyHomePageState extends State<MyHomePage> {
     return Container(child: game);
   }
 }
-
-//        __               __       __  __    __  __     ________  ______  _______   __         ______   __      __  ________  _______                      __
-//       /  |__ __        /  \     /  |/  |  /  |/  |   /        |/      |/       \ /  |       /      \ /  \    /  |/        |/       \         __ __      /  |
-//      /$$//  /  |       $$  \   /$$ |$$ |  $$ |$$ |   $$$$$$$$/ $$$$$$/ $$$$$$$  |$$ |      /$$$$$$  |$$  \  /$$/ $$$$$$$$/ $$$$$$$  |       /  /  |    /$$/
-//     /$$/ $$ $$/_       $$$  \ /$$$ |$$ |  $$ |$$ |      $$ |     $$ |  $$ |__$$ |$$ |      $$ |__$$ | $$  \/$$/  $$ |__    $$ |__$$ |       $$ $$/_   /$$/
-//    /$$/ / $$$   |      $$$$  /$$$$ |$$ |  $$ |$$ |      $$ |     $$ |  $$    $$/ $$ |      $$    $$ |  $$  $$/   $$    |   $$    $$<       / $$$   | /$$/
-//   /$$/  $$$$$$$/       $$ $$ $$/$$ |$$ |  $$ |$$ |      $$ |     $$ |  $$$$$$$/  $$ |      $$$$$$$$ |   $$$$/    $$$$$/    $$$$$$$  |      $$$$$$$/ /$$/
-//  /$$/    /$$$  |       $$ |$$$/ $$ |$$ \__$$ |$$ |_____ $$ |    _$$ |_ $$ |      $$ |_____ $$ |  $$ |    $$ |    $$ |_____ $$ |  $$ |       /$$$  |/$$/
-// /$$/     $$/$$/        $$ | $/  $$ |$$    $$/ $$       |$$ |   / $$   |$$ |      $$       |$$ |  $$ |    $$ |    $$       |$$ |  $$ |       $$/$$//$$/
-// $$/                    $$/      $$/  $$$$$$/  $$$$$$$$/ $$/    $$$$$$/ $$/       $$$$$$$$/ $$/   $$/     $$/     $$$$$$$$/ $$/   $$/              $$/
-
-
